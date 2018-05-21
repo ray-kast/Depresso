154efb7ad15dbe74765ec4cfa4c6e1b62aa9a6b0
rookie1024 <rookie1286@gmail.com>
2018-05-21 12:54:38 -0700

    First commit!  Woo!

---
+++ Assets/Behaviors/FollowCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera)), DisallowMultipleComponent]
+public class FollowCam : MonoBehaviour {
+	public Transform target;
+	public float speed;
+	Camera cam;
+
+	void Awake() {
+		cam = GetComponent<Camera>();
+	}
+
+	void Update() {
+		var pos = Vector3.Lerp(target.position, cam.transform.position, Mathf.Exp(-Time.deltaTime * speed));
+
+		pos.z = cam.transform.position.z;
+
+		cam.transform.position = pos;
+	}
+}
---
+++ Assets/Behaviors/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D)), DisallowMultipleComponent]
+public class Player : MonoBehaviour {
+	// Use this for initialization
+	void Start() {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var body = GetComponent<Rigidbody2D>();
+
+		var vel = body.velocity;
+
+		vel.x = Input.GetAxis("Horizontal") * 5.0f;
+
+		if (Input.GetAxis("Jump")) vel.y = 1.0f;
+
+		body.velocity = vel;
+	}
+}
