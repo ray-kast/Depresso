ff67c17647ee33d1603cd143fa12f6a79205f83c
rookie1024 <rookie1286@gmail.com>
2018-05-21 13:31:46 -0700

    Added a game manager and some blank scripts.

---
+++ Assets/Behaviors/Loader.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+	public GameObject gameManager;
+
+	void Awake() {
+		if (GameManager.Instance == null) Instantiate(gameManager);
+	}
+}
