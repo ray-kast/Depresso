b6d1e0b1ac1862952d1deca2b1f8a2977431c89b
rookie1024 <rookie1286@gmail.com>
2018-06-04 11:36:28 -0700

    Improved playability.

--- Assets/Behaviors/Player.cs
+++ Assets/Behaviors/Player.cs
@@ -9,53 +9,69 @@ public class Player : MonoBehaviour {
   public bool controlsEnabled;
 
   BoxCollider2D box;
-  bool onGround, wasOnGround;
+  bool doJump, onGround, wasOnGround;
   int jumpsLeft;
+  float move;
 
   void Awake() {
     box = GetComponent<BoxCollider2D>();
     onGround = false;
     wasOnGround = false;
     jumpsLeft = airJumps;
+    move = 0.0f;
   }
 
   void FixedUpdate() {
-    var hits = new RaycastHit2D[1];
+    {
+      var hits = new RaycastHit2D[1];
 
-    var size = box.size;
-    size.Scale(transform.localScale);
+      var size = box.size;
+      size.Scale(transform.localScale);
 
-    const float HEIGHT = 0.1f;
+      const float HEIGHT = 0.1f;
 
-    // Assuming for now that box.offset == 0
-    var pos = new Vector2(transform.position.x, transform.position.y - 0.5f * (size.y - HEIGHT));
+      // Assuming for now that box.offset == 0
+      var pos = new Vector2(transform.position.x, transform.position.y - 0.5f * (size.y - HEIGHT));
 
-    onGround = Physics2D.BoxCast(pos, new Vector2(size.x, HEIGHT), 0.0f,
-      Vector2.down, new ContactFilter2D {
-        layerMask = ~LayerMask.GetMask("Player"),
-        useDepth = false,
-        useLayerMask = true,
-        useNormalAngle = false,
-        useTriggers = false,
-      }, hits, 0.001f) > 0;
+      onGround = Physics2D.BoxCast(pos, new Vector2(size.x, HEIGHT), 0.0f,
+        Vector2.down, new ContactFilter2D {
+          layerMask = ~LayerMask.GetMask("Player"),
+          useDepth = false,
+          useLayerMask = true,
+          useNormalAngle = false,
+          useTriggers = false,
+        }, hits, 0.001f) > 0;
 
-    wasOnGround = onGround;
+      wasOnGround = onGround;
+    }
+
+    {
+      var body = GetComponent<Rigidbody2D>();
+      var pos = body.position;
+      var vel = body.velocity;
+
+      if (doJump) {
+        doJump = false;
+        vel.y = jumpPower;
+      }
+
+      pos.x += move * Time.deltaTime;
+
+      body.position = pos;
+      body.velocity = vel;
+    }
   }
 
   void Update() {
     var body = GetComponent<Rigidbody2D>();
 
-    var vel = body.velocity;
-
-    vel.x = Mathf.Lerp(Input.GetAxisRaw("Horizontal") * speed * (controlsEnabled ? 1.0f : 0.0f), vel.x, Mathf.Exp(-Time.deltaTime * pickup));
+    move = Mathf.Lerp(Input.GetAxisRaw("Horizontal") * speed * (controlsEnabled ? 1.0f : 0.0f), move, Mathf.Exp(-Time.deltaTime * pickup));
 
     if (controlsEnabled && GameManager.Instance.GetAxisDownPos("Jump")) {
-      if (onGround || jumpsLeft > 0) vel.y = jumpPower;
+      if (onGround || jumpsLeft > 0) doJump = true;
 
       if (onGround) jumpsLeft = airJumps;
       else if (jumpsLeft > 0) --jumpsLeft;
     }
-
-    body.velocity = vel;
   }
 }
