50e447d081b06f3f66879684e8f494cdc0b466e6
rookie1024 <rookie1286@gmail.com>
2018-06-06 21:51:02 -0700

    Prevented Friend from softlocking the game.

--- Assets/Behaviors/Friend.cs
+++ Assets/Behaviors/Friend.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Friend : MonoBehaviour {
+  bool triggered = false;
+
+  void Awake() {
+    triggered = false;
+  }
+
+  IEnumerator InteractionSequence(Player player) {
+    player.controlsEnabled = false;
+
+    yield return new WaitForSeconds(2.0f);
+
+    player.controlsEnabled = true;
+    triggered = true;
+  }
+
   void OnInteract(GameObject from) {
+    if (triggered) return; // Only do this once
+
     var player = from.GetComponent<Player>();
 
     if (player == null) return;
 
-    player.controlsEnabled = false;
+    StartCoroutine(InteractionSequence(player));
   }
 }
